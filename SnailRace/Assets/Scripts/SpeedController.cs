e8d9223ca707d0991020abbc09c4a5b266ef265b
mraue <martin.raue@gmail.com>
2017-04-29 14:44:14 +0200

    Base

---
+++ SnailRace/Assets/Scripts/SpeedController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedController : MonoBehaviour
+{
+	public float speedIncrease = 0.03f;
+	public float friction = 0.05f;
+
+	public Image image;
+
+	public float currentSpeed;
+
+	public void OnSpeedIncrease()
+	{
+		currentSpeed += speedIncrease;
+		currentSpeed = Mathf.Min(1f, currentSpeed);
+	}
+
+	void Update()
+	{
+		currentSpeed -= friction * Time.deltaTime;
+		currentSpeed = Mathf.Max(0f, currentSpeed);
+		image.fillAmount = currentSpeed;
+	}
+}
--- SnailRace/Assets/Scripts/TypeInputController.cs
+++ SnailRace/Assets/Scripts/TypeInputController.cs
@@ -1,19 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TypeInputController : MonoBehaviour
 {
-	void Start()
-	{
+	public const string BOOK_PATH = "Texts/jule_verne_around_the_moon";
+	public const string HIGHLIGHTED_CHARACTER = "<b><color=magenta>{0}</color></b>";
 
+	public Text label;
+	public UnityEvent onCharacterTyped;
+
+	StringReader _reader;
+
+	int _currentIndex;
+	string _line;
+	string _currentCharacter;
+	string _displayLine;
+
+	void Awake()
+	{
+		var asset = Resources.Load(BOOK_PATH) as TextAsset;
+		_reader = new StringReader(asset.text);
 	}
 
 	void Update()
 	{
-		if (Input.GetKeyDown("t"))
+		if (_line == null)
 		{
-			Debug.Log("Test");
+			NextLine();
 		}
+
+		if (Input.inputString.ToLower() == _line[_currentIndex].ToString().ToLower())
+		{
+			onCharacterTyped.Invoke();
+
+			_currentIndex += 1;
+
+			if (_currentIndex < _line.Length - 1)
+			{
+				label.text = GetFormattedLine(_line, _currentIndex);
+			}
+			else
+			{				
+				NextLine();
+			}
+		}
+	}
+
+	void NextLine()
+	{
+		_line = null;
+
+		while (string.IsNullOrEmpty(_line))
+		{			
+			_line = _reader.ReadLine().Trim();
+
+			if (_line == null)
+			{
+				break;
+			}
+		}
+
+		_currentIndex = 0;
+		label.text = GetFormattedLine(_line, _currentIndex);
+	}
+
+	string GetFormattedLine(string line, int index)
+	{
+		var character = _line[index];
+		character = character == ' ' ? '_' : character;
+		return _line.Substring(0, index)
+			        + string.Format(HIGHLIGHTED_CHARACTER, character)
+					+ _line.Substring(index + 1, _line.Length - index - 1);
 	}
 }
