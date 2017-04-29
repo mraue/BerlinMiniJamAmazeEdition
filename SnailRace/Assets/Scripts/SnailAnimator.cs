40d11db7f683dcb057eef7a4257e480214f73473
mraue <martin.raue@gmail.com>
2017-04-29 16:20:56 +0200

    Scafffolded snail animator

---
+++ SnailRace/Assets/Scripts/SnailAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class SnailAnimator : MonoBehaviour
+{
+	public float minSpeed;
+	public SpeedController speedController;
+	bool _isAnimating;
+
+	void Updated()
+	{
+		if (speedController.currentSpeed < minSpeed && _isAnimating)
+		{
+			StopAnimation();
+		}
+		else if (!_isAnimating)
+		{
+			StartAnimation();
+		}
+	}
+
+	void StopAnimation()
+	{
+		
+	}
+
+	void StartAnimation()
+	{
+		
+	}
+}
