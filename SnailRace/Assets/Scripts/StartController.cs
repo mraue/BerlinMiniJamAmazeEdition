d161ae6045e51fae09ed0a554fe86ec1b99ff3c7
mraue <martin.raue@gmail.com>
2017-04-29 17:30:52 +0200

    Added Start screen

---
+++ SnailRace/Assets/Scripts/StartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StartController : MonoBehaviour
+{
+	public CanvasGroup canvasGroup;
+
+	public void OnStart()
+	{
+		canvasGroup.DOFade(0f, 1f).SetEase(Ease.Linear).OnComplete(() => { canvasGroup.gameObject.SetActive(false); });
+	}
+}
