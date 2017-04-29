185d6c1082943583e0b8fb9d140be83781f57c6e
mraue <martin.raue@gmail.com>
2017-04-29 16:07:01 +0200

    More balancing

--- SnailRace/Assets/Scripts/SpeedController.cs
+++ SnailRace/Assets/Scripts/SpeedController.cs
@@ -9,17 +9,25 @@ public class SpeedController : MonoBehaviour
 	public Image image;
 
 	public float currentSpeed;
+	float _currentFriction;
+	float _idleTime;
 
 	public void OnSpeedIncrease()
 	{
 		currentSpeed += speedIncrease;
 		currentSpeed = Mathf.Min(1f, currentSpeed);
+		_currentFriction = 0.3f * friction + 0.7f * currentSpeed * friction;
+		_idleTime = 0f;
 	}
 
 	void Update()
 	{
-		currentSpeed -= friction * Time.deltaTime;
+		currentSpeed -= _currentFriction * Time.deltaTime;
 		currentSpeed = Mathf.Max(0f, currentSpeed);
 		image.fillAmount = currentSpeed;
+		if (_idleTime > 1f)
+		{
+			_currentFriction = friction * 3f;
+		}
 	}
 }
